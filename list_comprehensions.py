83506f393f508e8841f9453deff1e0c87d3bec3a
Ruben Marquez <r.marquez.hdz@gmail.com>
2022-02-10 16:05:03 -0600

    Ahora solo usando list comprehensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,9 +1,5 @@
 def run():
-    squares = []
-    for i in range(1, 101):
-        if i % 3 != 0:
-            squares.append(i**2)
-            
+    squares = [i**2 for i in range(1, 101) if i % 3 != 0]
     print(squares)
         
 
