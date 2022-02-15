1b195dabba02dd0e431ccc987f6c1d6fddfcc1e7
Ruben Marquez <r.marquez.hdz@gmail.com>
2022-02-15 16:34:02 -0600

    ahora al 1000 con la reiz cuadrada

--- dic_comprehensions.py
+++ dic_comprehensions.py
@@ -1,6 +1,6 @@
 def run():
     #Reducioendo codigo y haciendo ahora raiz cuadrada
-    my_dict = {i : i**3 for i in range(1, 101) if i % 3 != 0}
+    my_dict = {i : round(i**0.5,4) for i in range(1, 1001) if i % 3 != 0}
     print(my_dict)  
 
 
